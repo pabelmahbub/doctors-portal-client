bb6264e973e5dc9d7a9fea95c826621cd3200bfd
Rahman Muhammad Mahbubur <43867380+pabelmahbub@users.noreply.github.com>
2022-08-29 00:01:09 +0900

    login form done

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,268 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import Footer from '../Shared/Footer';
+import auth from '../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from "react-hook-form";
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 
 function Login() {
+  const [signInWihGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+  
+  
+  if (loading || googleLoading) {
+    return <p>Loading...</p>;
+  }
+
+
+  if(googleUser){
+    console.log(googleUser);
+  }
+  
+  const submit = data =>{
+    console.log(data);
+    signInWithEmailAndPassword(data.email,data.password);
+  }
   return (
-    <div>
-      <h1>Login</h1>
+<>
+    <div className='flex h-screen justify-center items-center'>
+       <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="text-center text-2xl font-bold">Login</h2>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+
+          <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Email</span>
+              </label>
+              <input 
+              type="email" 
+              placeholder="Your email address" 
+              class="input input-bordered w-full max-w-xs"
+              {...register("email", {
+                        required:{
+                          value: true,
+                          message:'Email is Required'
+                        },
+                        pattern: {
+                          //value:/[a-z0-9]+@[a-z]+\.\.[a-z]{2,3}/,
+                          value: /\S+@\S+\.\S+/,
+                          message: 'Provide a valid email'
+                        }
+                      })}
+              />
+              <label class="label">
+              {errors.email?.type === 'required' &&  <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+              {errors.email?.type === 'pattern' &&  <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+              </label>
+            </div>
+
+            <div class="form-control w-full max-w-xs">
+              <label class="label">
+                <span class="label-text">Password</span>
+              </label>
+              <input 
+              type="password" 
+              placeholder="Passeword here" 
+              class="input input-bordered w-full max-w-xs"
+              {...register("password", {
+                        required:{
+                          value: true,
+                          message:'Password is Required'
+                        },
+                        minLength: {
+                          value:5,
+                          message:'Password must be 5 or more characters'
+                        }
+                      })}
+              />
+              <label class="label">
+              {errors.password?.type === 'required' &&  <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+              {errors.password?.type === 'minLength' &&  <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+              </label>
+            </div>
+
+
+            <input 
+                type='submit' 
+                value= 'login'
+                class="btn w-full max-w-xs text-white"
+                        />
+          </form>
+   
+
+
+        <div class="divider">OR</div>
+    <button 
+    onClick={() => signInWihGoogle()}
+    class="btn btn-outline">Continue with Google</button>
+  </div>
+</div>
+
+
     </div>
+    <Footer></Footer>
+   </>
   )
 }
 
 export default Login
+
+
+// import React from 'react';
+// import { useState } from 'react';
+// import Footer from '../Shared/Footer';
+// import auth from '../../firebase.init';
+// import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+// import { useForm } from "react-hook-form";
+// import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+
+// function Login() {
+//   const [signInWihGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+//   const [signInWithEmailAndPassword,user,loading,error] = useSignInWithEmailAndPassword(auth);
+//   const { register, handleSubmit, watch, formState: { errors } } = useForm();
+ 
+
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+
+
+//   const onSubmit = data => {
+//     console.log(data);
+//     signInWithEmailAndPassword(data.email,data.password);
+//   }
+  
+//   return (
+// <>
+//     <div className='flex h-screen justify-center items-center'>
+//        <div class="card w-96 bg-base-100 shadow-xl">
+//   <div class="card-body">
+//     <h2 class="text-center text-2xl font-bold">Login</h2>
+
+//     <form onSubmit={handleSubmit(onSubmit)}>
+
+//     <div class="form-control w-full max-w-xs">
+//   <label class="label">
+//     <span class="label-text">Email</span>
+//   </label>
+//   <input 
+//       type="email" 
+//       placeholder="Your email address" 
+//       class="input input-bordered w-full max-w-xs"
+//       {...register("email", {
+//         required:{
+//           value: true,
+//           message:'Email is Required'
+//         },
+//         pattern: {
+//           value:/[a-z0-9]+@[a-z]+\.edu\.[a-z]{2,3}/,
+//           message: 'Provide a valid email'
+//         }
+//       })}
+//    />
+//    <label class="label">
+//    {errors.email?.type === 'required' &&  <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+//    {errors.email?.type === 'patern' &&  <span class="label-text-alt text-red-500">{errors.email.message}</span>}
+//   </label>
+// </div>
+
+// <div class="form-control w-full max-w-xs">
+//   <label class="label">
+//     <span class="label-text">Password</span>
+//   </label>
+//   <input 
+//       type="password" 
+//       placeholder="Type password here" 
+//       class="input input-bordered w-full max-w-xs"
+//       {...register("password", {
+//         required:{
+//           value: true,
+//           message:'Passeord is Required'
+//         },
+//         minLength: {
+//           value:6,
+//           message: 'Must be 6 character or longer'
+//         }
+//       })}
+//    />
+//    <label class="label">
+//    {errors.password?.type === 'required' &&  <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+//    {errors.password?.type === 'minLength' &&  <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+//   </label>
+// </div>
+
+
+
+// {/* <div class="form-control w-full max-w-xs">
+//   <label class="label">
+//     <span class="label-text">Password</span>
+//   </label>
+//   <input 
+//       onChange={(e) => setPassword(e.target.value)}
+//       type="password" 
+//       placeholder="Please type your password" 
+//       class="input input-bordered w-full max-w-xs"
+//       {...register("password", {
+//         required:{
+//           value: 6,
+//           message:'Password is Required'
+//         },
+//         pattern: {
+//           value: /^(?=.*\d)(?=.*[a-zA-Z])[a-zA-Z0-9]{7,}$/,
+//           message: 'Must be 6 characters long and containing digits'
+//         }
+//       })}
+//    />
+//    <label class="label">
+//    {errors.password?.type === 'required' &&  <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+//    {errors.password?.type === 'pattern' &&  <span class="label-text-alt text-red-500">{errors.password.message}</span>}
+//   </label>
+// </div> */}
+
+
+
+
+      
+     
+//       {/* <button 
+//           type='submit' 
+//           class="btn w-full max-w-xs text-white"
+//           onClick={() => signInWithEmailAndPassword(email, password)}>Login</button> */}
+
+//      <input 
+//           type='submit' 
+//           value= 'login'
+//           class="btn w-full max-w-xs text-white"
+//           onClick={() => signInWithEmailAndPassword(email, password)} />
+//     </form>
+
+
+   
+
+
+//     <div class="divider">OR</div>
+//     <button 
+//     onClick={() => signInWihGoogle()}
+//     class="btn btn-outline">Continue with Google</button>
+//   </div>
+// </div>
+
+
+//     </div>
+//     <Footer></Footer>
+//    </>
+//   )
+// }
+
+// export default Login
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,20 +1,29 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import getAuth from 'firebase/auth';
+import {getAuth} from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:Process.env.REACT_APP_API_KEY,
-  authDomain: Process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: Process.env.REACT_APP_PROJECT_ID,
-  storageBucket: Process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: Process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: Process.env.REACT_APP_APP_ID
+
+  apiKey: "AIzaSyBOIjIpavTPSNzMcI5fbkqMu_X-YV3Z55c",
+  authDomain: "doctors-portal-3cd4f.firebaseapp.com",
+  projectId: "doctors-portal-3cd4f",
+  storageBucket: "doctors-portal-3cd4f.appspot.com",
+  messagingSenderId: "144696362141",
+  appId: "1:144696362141:web:2ae4a74aede80cabda0074"
+
+//   apiKey:process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 export default auth;
+
