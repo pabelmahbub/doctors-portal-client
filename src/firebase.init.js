eaba854068bbc905bc63d8d75eaeb6b9c234751e
Rahman Muhammad Mahbubur <43867380+pabelmahbub@users.noreply.github.com>
2022-08-29 10:13:52 +0900

    firebase.init env variable fix

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,19 +7,19 @@ import {getAuth} from 'firebase/auth';
 // Your web app's Firebase configuration
 const firebaseConfig = {
 
-  apiKey: "AIzaSyBOIjIpavTPSNzMcI5fbkqMu_X-YV3Z55c",
-  authDomain: "doctors-portal-3cd4f.firebaseapp.com",
-  projectId: "doctors-portal-3cd4f",
-  storageBucket: "doctors-portal-3cd4f.appspot.com",
-  messagingSenderId: "144696362141",
-  appId: "1:144696362141:web:2ae4a74aede80cabda0074"
+  // apiKey: "AIzaSyBOIjIpavTPSNzMcI5fbkqMu_X-YV3Z55c",
+  // authDomain: "doctors-portal-3cd4f.firebaseapp.com",
+  // projectId: "doctors-portal-3cd4f",
+  // storageBucket: "doctors-portal-3cd4f.appspot.com",
+  // messagingSenderId: "144696362141",
+  // appId: "1:144696362141:web:2ae4a74aede80cabda0074"
 
-//   apiKey:process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
